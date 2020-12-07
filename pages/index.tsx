1d63ef63681f6e7aad4798664aa2ffe8249b919e
moootoko <litt0rio.k1s0@gmail.com>
2020-12-08 01:06:09 +0900

    トップページ完成

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,64 @@
 import Head from 'next/head';
+import { Container, Row, Col } from 'react-bootstrap';
+import BaseLayout from '../components/baselayout';
 
 const IndexPage = () => (
   <>
     <Head>
       <title>もーとこぶろぐ</title>
+      <link href="https://fonts.googleapis.com/css2?family=Kosugi+Maru&display=swap" rel="stylesheet" />
     </Head>
-    <div className="container">
-      <h2 id="title" className="text-center">もーとこぶろぐ</h2>
-      <p className="text-center">このサイトはReact、Next.js、TypeScriptを学ぶために開設しました。</p>
-    </div>
+    <BaseLayout>
+      <div className="mt-3 mb-5 d-flex justify-content-center">
+        <h1>もーとこぶろぐ</h1>
+      </div>
+      <Container>
+        <Row className="mb-5 d-flex justify-content-center">
+          <Col sm={4}>
+            {
+              [
+                'このサイトはReact, Next.js, TypeScriptを学ぶために開設しました。',
+                '仕事で使えるくらいまでスキルアップしたいです。'
+              ].map((msg: string, idx: number) => <p key={idx}>{msg}</p>)
+            }
+          </Col>
+          <Col sm={4}>
+            <span className="d-flex justify-content-center">
+              <img src="/react.svg" alt="ReactLogo" className="logo" id="react-logo" />
+            </span>
+          </Col>
+        </Row>
+        <Row className="d-flex justify-content-center">
+          <Col sm={4}>
+            {
+              [
+                'でも一番好きなのはデータベースです。',
+                'データベーススペシャリストに、おれはなる！！'
+              ].map((msg: string, idx: number) => <p key={idx}>{msg}</p>)
+            }
+          </Col>
+          <Col sm={4}>
+            <span className="d-flex justify-content-center">
+              <img src="/mariadb.svg" alt="MariaDB" className="logo" id="dbLogo" />
+            </span>
+          </Col>
+        </Row>
+      </Container>
+    </BaseLayout>
+    <style jsx>{`
+      .logo {
+        max-width: 50%;
+        height: auto;
+        opacity: 0.3;
+      }
+      #react-logo {
+        animation: 5s linear infinite rotation1;
+      }
+      @keyframes rotation1{
+        0%{ transform:rotate(0);}
+        100%{ transform:rotate(360deg); }
+      }
+    `}</style>
   </>
 );
 
