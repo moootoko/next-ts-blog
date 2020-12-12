2c9bf0ebdda68f7ee7419d37e09dcf28f195f9e6
moootoko <litt0rio.k1s0@gmail.com>
2020-12-13 02:21:04 +0900

    モバイル用にデザイン微修正

--- pages/VideoModalTest.tsx
+++ pages/VideoModalTest.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Row, Col, Button, Modal } from 'react-bootstrap';
+import { Container, Row, Col, Button, Modal } from 'react-bootstrap';
 import BaseLayout from '../components/baselayout';
 
 const VideoModalTest = () => {
@@ -16,25 +16,26 @@ const VideoModalTest = () => {
       <Button variant="primary" onClick={handleShow}>
         Launch demo modal
       </Button>
-
       <Modal size="xl" show={show} onHide={handleClose} centered>
         <Modal.Body>
-          <Row className="mb-3">
-            <Col className="d-flex justify-content-center">
-              <iframe width={width} height={height} src="https://www.youtube.com/embed/TtEtYRZZW3g" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-            </Col>
-            <Col className="d-flex justify-content-center">
-              <iframe width={width} height={height} src="https://www.youtube.com/embed/G_T9MeJQetU" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-            </Col>
-          </Row>
-          <Row>
-            <Col className="d-flex justify-content-center">
-              <iframe width={width} height={height} src="https://www.youtube.com/embed/H4RtkC2JwQ0" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-            </Col>
-            <Col className="d-flex justify-content-center">
-              <iframe width={width} height={height} src="https://www.youtube.com/embed/8a1bbifkiRI" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-            </Col>
-          </Row>
+          <Container>
+            <Row className="mb-lg-3">
+              <Col xs={12} lg={6} className="d-flex justify-content-center mb-2 mb-lg-0">
+                <iframe width={width} height={height} src="https://www.youtube.com/embed/TtEtYRZZW3g" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+              </Col>
+              <Col xs={12} lg={6} className="d-flex justify-content-center mb-2 mb-lg-0">
+                <iframe width={width} height={height} src="https://www.youtube.com/embed/G_T9MeJQetU" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+              </Col>
+            </Row>
+            <Row>
+              <Col xs={12} lg={6} className="d-flex justify-content-center mb-2 mb-lg-0">
+                <iframe width={width} height={height} src="https://www.youtube.com/embed/H4RtkC2JwQ0" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+              </Col>
+              <Col xs={12} lg={6} className="d-flex justify-content-center mb-2 mb-lg-0">
+                <iframe width={width} height={height} src="https://www.youtube.com/embed/8a1bbifkiRI" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+              </Col>
+            </Row>
+          </Container>
         </Modal.Body>
       </Modal>
     </BaseLayout>
