e760d7a9fe708c27e411c817f0de464125227f77
moootoko <litt0rio.k1s0@gmail.com>
2020-12-13 01:22:55 +0900

    不要ファイル削除

--- pages/blog.tsx
+++
@@ -1,5 +0,0 @@
-const Blog = () => (
-  <h2>This is Blog page.</h2>
-)
-
-export default Blog;
--- pages/hololive.tsx
+++
@@ -1,5 +0,0 @@
-const Hololive = () => (
-  <h2>This is Hololive page.</h2>
-)
-
-export default Hololive;
