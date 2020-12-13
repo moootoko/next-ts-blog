f87318cee259d887577f28c4d9472c6a398a7cd1
moootoko <litt0rio.k1s0@gmail.com>
2020-12-13 22:36:39 +0900

    切り抜きデータ追加

--- utils/hololive/clips-data.ts
+++ utils/hololive/clips-data.ts
@@ -1,16 +1,32 @@
 export type Clips = {
   memberName: string,
-  clipsId: string[]
+  id: string[]
 };
 
 const clipsData: Clips[] = [
   {
     memberName: 'HoshimachiSuisei',
-    clipsId: ['_8eUrkYNnLY', 'WQeu3TRtm9o', 'gFRZQ1HwqBs', 'NraFKiafr8I']
+    id: ['_8eUrkYNnLY', 'WQeu3TRtm9o', 'gFRZQ1HwqBs', 'NraFKiafr8I']
   },
   {
     memberName: 'Robocosan',
-    clipsId: ['fY1CQ5kSRxY', 'bFSzljEMydg', 'Hhc1DahaRSg', 'TTBz820stS0']
+    id: ['fY1CQ5kSRxY', 'bFSzljEMydg', 'Hhc1DahaRSg', 'TTBz820stS0']
+  },
+  {
+    memberName: 'SakuraMiko',
+    id: ['X9zw0QF12Kc', 'WqWqN10biP8', 'f2LPs6JBbjw', 'GSvtZqD-ZeM']
+  },
+  {
+    memberName: 'TokinoSora',
+    id: ['NT6Pf28eCgQ', 'dH0eSrwHF9o', '3ekVHzjfSCQ', 'oDzugRtW2gw']
+  },
+  {
+    memberName: 'AkaiHaato',
+    id: ['NvOHijJqups', 'c001nMFyex8', 'rC99t6VjrgY', '']
+  },
+  {
+    memberName: 'AkiRosenthal',
+    id: ['NvOHijJqups', 'c001nMFyex8', 'rC99t6VjrgY', '']
   },
 ]
 
