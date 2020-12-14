61d889bb66ada68249825d1d9e450cc37c81b8fa
moootoko <litt0rio.k1s0@gmail.com>
2020-12-15 01:29:54 +0900

    スタイルを修正

--- components/hololive/portrait.tsx
+++ components/hololive/portrait.tsx
@@ -26,11 +26,6 @@ const Portrait = ({memberName}: Props) => (
         border-radius: .5rem;
       }
       .text1 {
-        position: absolute;
-        left: 0;
-        right: 0;
-        bottom: 0;
-        background-color: rgba(255, 255, 255, 0.9);
         font-size: 0.8rem;
       }
     `}</style>
