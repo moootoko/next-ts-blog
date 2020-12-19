c02d856095070a7a6d1c98ce94ae79c683854646
moootoko <litt0rio.k1s0@gmail.com>
2020-12-20 00:04:43 +0900

    YouTubeボタン、Twitterボタンのコンポーネントを追加

---
+++ components/hololive/external-site-buttons.tsx
@@ -0,0 +1,65 @@
+import Link from 'next/link';
+import { Card } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons';
+
+type Props = {
+  id: string,
+  name: string
+}
+
+export const TwitterButton = ({ id, name }: Props) => (
+  <>
+    <Link href={'https://twitter.com/' + id}>
+      <a target="_blank" rel="noopener noreferrer">
+        <Card>
+          <div className="d-flex">
+            <div className="d-flex align-items-center px-2">
+              <FontAwesomeIcon icon={faTwitter} size="2x" style={{ color: 'rgb(0, 172, 237)' }} />
+            </div>
+            <div className="p-2">
+              <div className="channel-name">{name}</div>
+              <div className="channel-id">{id}</div>
+            </div>
+          </div>
+        </Card>
+      </a>
+    </Link>
+    <style jsx>{`
+      .channel-name {
+        font-weight: bold;
+      }
+      .channel-id {
+        font-size: 0.7rem;
+      }
+    `}</style>
+  </>
+);
+
+export const YoutubeButton = ({ id, name }: Props) => (
+  <>
+    <Link href={'https://www.youtube.com/channel/' + id}>
+      <a target="_blank" rel="noopener noreferrer">
+        <Card>
+          <div className="d-flex">
+            <div className="d-flex align-items-center px-2">
+              <FontAwesomeIcon icon={faYoutube} size="2x" style={{ color: 'rgb(255, 0, 0)' }} />
+            </div>
+            <div className="p-2">
+              <div className="channel-name">{name}</div>
+              <div className="channel-id">{id}</div>
+            </div>
+          </div>
+        </Card>
+      </a>
+    </Link>
+    <style jsx>{`
+      .channel-name {
+        font-weight: bold;
+      }
+      .channel-id {
+        font-size: 0.7rem;
+      }
+    `}</style>
+  </>
+);
